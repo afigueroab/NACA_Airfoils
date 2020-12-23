77122eab2bb5fb6c608a3dca86e5349476b90d59
afigueroab <p.andres.figueroa@gmail.com>
2020-12-23 14:00:42 -0600

    created a main py file and small changes to notebook

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import csv
+
+import xlrd
+import pandas as pd
+import numpy as np
+
+def naca_export():
+    """[summary]
+
+    Args:
+        arg1 ([type]): [description]
+        arg2 ([type]): [description]
+    """
+
+if __name__ == "__main__":
+    naca_export()
